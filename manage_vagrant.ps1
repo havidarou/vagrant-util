17bf7d2c2bb932a1a154e1705907a9628fab29b6
havidarou <havidarou@gmail.com>
2021-05-05 12:42:54 +0200

    hyper-v version

--- manage_vagrant.ps1
+++ manage_vagrant.ps1
@@ -1,31 +1,20 @@
 #------------------------- Gather parameters --------------------------#
 
 param (
-    [switch]$reset,
-    [switch]$help,
     [switch]$list,
     [string]$os = "ubuntu-18.04",
+    [string]$base_path = "C:\Users\havidarou\",
     [string]$memory = "1024",
-    [string]$base_path = "C:\Users\havida\",
-    [string]$wsl_base_path = "/mnt/c/Users/havida/",
     [string]$cpu = "1",
     [string]$halt = "",
     [string]$ssh = "",
-    [string]$version = "",
     [string]$name = "",
-    [string]$destroy = "",
-    [string]$deploy = ""
+    [string]$destroy = ""
 )
 
 #------------------------- Gather parameters --------------------------#
 
 # ------------------------- Common functions ------------------------- #
-
-# Usage
-function Usage
-{
-    "Usage"
-}
 
 # Init vagrant file
 function init
@@ -37,47 +26,19 @@ function init
 Vagrant.configure("2") do |config|
 
     config.vm.box = "MY_FAMILY/MY_OS"
-    config.vm.synced_folder ".", "/vagrant", group:"root", owner:"root", mount_options: ["dmode=777,fmode=777"]
-
-    #config.vm.provision "shell", inline: "netsh advfirewall set allprofiles state off"
-
-    #config.vm.provision "shell", inline: "echo nameserver 8.8.8.8 > /etc/resolv.conf && cat /vagrant/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys"
-
-    #config.vm.provision "shell", inline: "curl -sSL https://get.docker.com/ | sh && curl -L https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose && ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose && echo vm.max_map_count=262144 >> /etc/sysctl.conf && sysctl -w vm.max_map_count=262144 && systemctl enable docker && systemctl start docker"
-
-    config.vm.network "private_network", ip: "MY_IP"
-
+    config.vm.synced_folder ".", "/vagrant", type: "smb", smb_username:"havidarou", smb_password:"Hitojanaiyo32!"
     config.vm.define "VAGRANT_NAME"
+    config.vm.hostname = "MY_NAME"
+    config.vm.network "public_network", bridge: "Default Switch"
 
-    config.vm.provider "virtualbox" do |vb|
-        vb.memory = "MY_MEMORY"
-        vb.cpus = "MY_CPU"
-        vb.name = "MY_NAME"
-        vb.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate//vagrant", "1"]
-        #vb.customize ["modifyvm", :id, "--clipboard", "bidirectional"]
+    config.vm.provider "hyperv" do |hv|
+        hv.memory = "MY_MEMORY"
+        hv.cpus = "MY_CPU"
+        hv.vmname = "MY_NAME"
     end
 
-    config.vm.hostname = "MY_NAME"
 end'
     return $vagrantfile
-}
-
-# Destroy
-function destroy
-{
-    cd $destroy
-    vagrant halt
-    vagrant destroy -f
-
-    # Get VM IP
-    $id = Get-Content .\Vagrantfile | findstr config.vm.network
-    $id = $id.split(" ")[-1].trim('"')
-
-    cd ..
-    rmdir $destroy -Force -Recurse
-
-    # Remove instance from ansible server hosts
-    wsl sudo sed -i "/$id/d" /etc/ansible/hosts
 }
 
 # Configure vagrant box
@@ -87,8 +48,6 @@ function configure
 
     # Windows like system
     if ($os -like "*win*") {
-        $vagrantfile = $vagrantfile -replace '#config.vm.provision "shell", inline: "netsh', 'config.vm.provision "shell", inline: "netsh'
-        $vagrantfile = $vagrantfile -replace '#vb.customize', 'vb.customize'
         if ($os -eq "windows-2012") {
             $vagrantfile = $vagrantfile -replace 'MY_OS', "Windows2012R2"
             $vagrantfile = $vagrantfile -replace 'MY_FAMILY', "mwrock"
@@ -122,17 +81,10 @@ function configure
     else {
         $vagrantfile = $vagrantfile -replace 'MY_OS', "$os"
         $vagrantfile = $vagrantfile -replace 'MY_FAMILY', "bento"
-
-        $vagrantfile = $vagrantfile -replace '#config.vm.provision "shell", inline: "echo', 'config.vm.provision "shell", inline: "echo'
-
-        if ($deploy -eq "docker") {
-            $vagrantfile = $vagrantfile -replace '#config.vm.provision "shell", inline: "curl', 'config.vm.provision "shell", inline: "curl'
-        }
     }
 
-    $vagrantfile = $vagrantfile -replace 'MY_IP', "11.0.0.$id"
-    $vagrantfile = $vagrantfile -replace 'MY_NAME', "$name-$id-$os"
-    $vagrantfile = $vagrantfile -replace 'VAGRANT_NAME', "$id-$os"
+    $vagrantfile = $vagrantfile -replace 'MY_NAME', "$name-$os"
+    $vagrantfile = $vagrantfile -replace 'VAGRANT_NAME', "$name-$os"
     $vagrantfile = $vagrantfile -replace 'MY_MEMORY', "$memory"
     $vagrantfile = $vagrantfile -replace 'MY_CPU', "$cpu"
 
@@ -150,17 +102,6 @@ cd $base_path\vagrant-util
 # If name was provided, generate vm
 if ($name -ne "") {
 
-    # Read stored ID
-    if (Test-Path .\storedId) {
-        $storedId = Get-Content .\storedId
-    } else {
-        $storedId = 3
-    }
-
-    # Increase ID counter
-    $id = [int]$storedId + 1
-    Set-Content -Path .\storedId -Value $id
-
     # Configure vagrantfile
     $vagrantfile = configure
 
@@ -171,36 +112,10 @@ if ($name -ne "") {
     # Write Vagrantfile to VM folder
     Set-Content -Path .\$folderName\Vagrantfile -Value $vagrantfile
 
-    # Copy ansible keys
-    cp $base_path\DATA\keys\id_rsa.pub $folderName\
-
-    # Add new instance to ansible server hosts
-    wsl echo "11.0.0.$id ansible_ssh_user=vagrant" | wsl sudo tee -a /etc/ansible/hosts
-
     # Start vagrant VM
     cd $folderName
     vagrant up
     cd $current_path
-
-    if ($deploy -eq "ansible") {
-        # Clone wazuh-ansible repository
-        git clone  --single-branch --branch master https://github.com/wazuh/wazuh-ansible $base_path\vagrant-util\$folderName\wazuh-ansible
-
-        # Add target host to playbook
-        (Get-Content "$base_path\vagrant-util\$folderName\wazuh-ansible\playbooks\wazuh-odfe-single.yml") -replace("<your server host>", "11.0.0.$id") | Set-Content "$base_path\vagrant-util\$folderName\wazuh-ansible\playbooks\wazuh-odfe-single.yml"
-
-        # Execute playbook
-        wsl ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook $wsl_base_path/vagrant-util/$folderName/wazuh-ansible/playbooks/wazuh-odfe-single.yml --become
-    }
-
-    if ($deploy -eq "docker") {
-        # Download docker-compose.yml file
-        if ($version -ne "") {
-            wsl curl -so $wsl_base_path/vagrant-util/$folderName/docker-compose.yml https://raw.githubusercontent.com/wazuh/wazuh-docker/$version/docker-compose.yml
-        } else {
-            wsl curl -so $wsl_base_path/vagrant-util/$folderName/docker-compose.yml https://raw.githubusercontent.com/wazuh/wazuh-docker/master/docker-compose.yml
-        }
-    }
 }
 # Otherwise, consider other commands
 else {
@@ -223,7 +138,12 @@ else {
 
     # Clean vagrant VM
     if ($destroy -ne "") {
-        destroy
+        cd $destroy
+        vagrant halt
+        vagrant destroy -f
+
+        cd ..
+        rmdir $destroy -Force -Recurse
         cd $current_path
         exit
     }
@@ -231,20 +151,6 @@ else {
     # List vagrant VMs
     if ($list.isPresent) {
         vagrant global-status --prune
-        cd $current_path
-        exit
-    }
-
-    # Reset counter
-    if ($reset.isPresent) {
-        Set-Content -Path .\storedId -Value 3
-        cd $current_path
-        exit
-    }
-
-    # Print usage
-    if ($usage.isPresent) {
-        usage
         cd $current_path
         exit
     }
